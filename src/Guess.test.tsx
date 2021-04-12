2e8fe894e62b0b891fadce068176638402d1bea5
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2021-04-12 06:49:44 -0300

    test(): remove unnecessary async and await from tests.

--- src/Guess.test.tsx
+++ src/Guess.test.tsx
@@ -4,64 +4,68 @@ import * as randomizer from './application/Randomizer';
 import { GAMES } from '../tests/fixtures/games';
 
 describe('Guess', () => {
-    it('should render game guess with only screenshot, input guess and pristine guess status', async () => {
+
+    it('should render game guess with only screenshot, input guess and pristine guess status', () => {
         randomizer.getRandomIntBetween = jest.fn(() => 1);
         const { asFragment, getByTestId } = render(<Guess options={ GAMES } />);
         expect(asFragment()).toMatchSnapshot();
-        expect(await getByTestId("guess-status")).toBeVisible();
+        expect(getByTestId("guess-status")).toBeVisible();
     });
-    it('should render error message and keep the same game with wrong guess', async () => {
+
+    it('should render error message and keep the same game with wrong guess', () => {
         // TODO: use real impl to simulate random pick and make sure game is not changed
         randomizer.getRandomIntBetween = jest.fn(() => 1);
         const { getByTestId, queryByTestId } = render(<Guess options={ GAMES } />);
-        const inputGuess = getByTestId(/txtGuess/i);
+        const inputGuess = getByTestId(/guess-input/i);
         fireEvent.change(inputGuess, { target: { value: 'wrong guess' } });
         fireEvent.blur(inputGuess);
-        expect(await getByTestId(/guess-status-fail/i)).toBeVisible();
-        expect(await queryByTestId(/next-button/i)).not.toBeInTheDocument();
+        expect(getByTestId(/guess-status-fail/i)).toBeVisible();
+        expect(queryByTestId(/next-button/i)).not.toBeInTheDocument();
     });
-    it('should render game information, success indication and show next game button with right guess', async () => {
+
+    it('should render game information, success indication and show next game button with right guess', () => {
         randomizer.getRandomIntBetween = jest.fn(() => 1);
         const { getByTestId, getByText, queryByTestId } = render(<Guess options={ GAMES } />);
-        const inputGuess = getByTestId(/txtGuess/i);
+        const inputGuess = getByTestId(/guess-input/i);
         fireEvent.change(inputGuess, { target: { value: 'golden axe' } });
         fireEvent.blur(inputGuess);
-        expect(await getByText(/Golden Axe/i)).toBeVisible();
-        expect(await getByText(/SMS/i)).toBeVisible();
-        expect(await getByText(/✔️/i)).toBeVisible(); // TODO: trocar para o testid
-        expect(await queryByTestId(/next-button/i)).toBeInTheDocument();
+        expect(getByText(/Golden Axe/i)).toBeVisible();
+        expect(getByText(/SMS/i)).toBeVisible();
+        expect(queryByTestId(/guess-status-success/i)).toBeVisible();
+        expect(queryByTestId(/next-button/i)).toBeInTheDocument();
     });
 
-    it('should render game information with minimum 3 character right guess', async () => {
+    it('should render game information with minimum 3 character right guess', () => {
         randomizer.getRandomIntBetween = jest.fn(() => 1);
         const { getByTestId, getByText } = render(<Guess options={ GAMES } />);
-        const inputGuess = getByTestId(/txtGuess/i);
+        const inputGuess = getByTestId(/guess-input/i);
         fireEvent.change(inputGuess, { target: { value: 'axe' } });
         fireEvent.blur(inputGuess);
-        expect(await getByText(/Golden Axe/i)).toBeVisible();
-        expect(await getByText(/SMS/i)).toBeVisible();
-    });
-    it('should not render game information with less than 3 character guess', async () => {
-        randomizer.getRandomIntBetween = jest.fn(() => 1);
-        const { getByTestId, getByText } = render(<Guess options={ GAMES } />);
-        const inputGuess = getByTestId(/txtGuess/i);
-        fireEvent.change(inputGuess, { target: { value: 'ax' } });
-        fireEvent.blur(inputGuess);
-        const errorMessage = await getByText(/❌/i); // TODO: trocar para o testid
-        expect(errorMessage).toBeVisible();
+        expect(getByText(/Golden Axe/i)).toBeVisible();
+        expect(getByText(/SMS/i)).toBeVisible();
     });
 
-    it('should reset screen information when click next after successfull guess', async () => {
+    it('should not render game information with less than 3 character guess', () => {
         randomizer.getRandomIntBetween = jest.fn(() => 1);
         const { getByTestId, queryByTestId } = render(<Guess options={ GAMES } />);
-        const inputGuess = getByTestId(/txtGuess/i);
+        const inputGuess = getByTestId(/guess-input/i);
+        fireEvent.change(inputGuess, { target: { value: 'ax' } });
+        fireEvent.blur(inputGuess);
+        expect(queryByTestId(/guess-status-fail/i)).toBeVisible();
+    });
+
+    it('should reset screen information when click next after successfull guess', () => {
+        randomizer.getRandomIntBetween = jest.fn(() => 1);
+        const { getByTestId, queryByTestId } = render(<Guess options={ GAMES } />);
+        const inputGuess = getByTestId(/guess-input/i);
         fireEvent.change(inputGuess, { target: { value: 'golden axe' } });
         fireEvent.blur(inputGuess);
         //
         fireEvent.click(getByTestId(/next-button/i));
-        expect(await queryByTestId(/guess-status-success/i)).not.toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-fail/i)).not.toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-pristine/i)).toBeInTheDocument();
-        expect(await getByTestId(/txtGuess/i).value).toEqual("");
+        expect(queryByTestId(/guess-status-success/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-fail/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-pristine/i)).toBeInTheDocument();
+        expect(inputGuess.value).toEqual("");
     });
+
 });
--- src/GuessStatus.test.tsx
+++ src/GuessStatus.test.tsx
@@ -2,27 +2,27 @@ import { render } from '@testing-library/react';
 import GuessStatus from './GuessStatus';
 
 describe('GuessStatus', () => {
-    it('should have pristine indication by default', async () => {
+    it('should have pristine indication by default', () => {
         const { queryByTestId } = render(<GuessStatus/>);
 
-        expect(await queryByTestId(/guess-status-pristine/i)).toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-success/i)).not.toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-fail/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-pristine/i)).toBeInTheDocument();
+        expect(queryByTestId(/guess-status-success/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-fail/i)).not.toBeInTheDocument();
     });
 
-    it('should have success indication with guessed true', async () => {
+    it('should have success indication with guessed true', () => {
         const { queryByTestId } = render(<GuessStatus guessed={ true }/>);
 
-        expect(await queryByTestId(/guess-status-success/i)).toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-fail/i)).not.toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-pristine/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-success/i)).toBeInTheDocument();
+        expect(queryByTestId(/guess-status-fail/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-pristine/i)).not.toBeInTheDocument();
     });
 
-    it('should have fail indication with guessed false', async () => {
+    it('should have fail indication with guessed false', () => {
         const { queryByTestId } = render(<GuessStatus guessed={ false }/>);
 
-        expect(await queryByTestId(/guess-status-success/i)).not.toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-pristine/i)).not.toBeInTheDocument();
-        expect(await queryByTestId(/guess-status-fail/i)).toBeInTheDocument();
+        expect(queryByTestId(/guess-status-success/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-pristine/i)).not.toBeInTheDocument();
+        expect(queryByTestId(/guess-status-fail/i)).toBeInTheDocument();
     });
 });
