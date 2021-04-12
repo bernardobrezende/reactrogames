e1211dd76fd67c22863e9578bc3823edff10ea0c
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2021-04-12 06:49:19 -0300

    refactor(Guess): extract handler function and change component style order.

--- src/Guess.tsx
+++ src/Guess.tsx
@@ -9,6 +9,10 @@ const MIN_GUESS_LENGTH : number = 3;
 
 export default function Guess({ options } : { options: InferProps<typeof Guess.propTypes> }) {
 
+    const [ guessed, setGuessed ] = useState<boolean | null>();
+    const [ currentGame, setCurrentGame ] = useState<GameModel>(pickGame());
+    const [ guessText, setGuessText ] = useState<string>("");
+
     function pickGame() : GameModel {
         return new GamePicker((options as GameModel[])).pick();
     };
@@ -19,21 +23,18 @@ export default function Guess({ options } : { options: InferProps<typeof Guess.p
         setGuessText("");
     };
 
-    const selectedGame = pickGame();
-    const [ guessed, setGuessed ] = useState<boolean | null>();
-    const [ currentGame, setCurrentGame ] = useState<GameModel>(selectedGame);
-    const [ guessText, setGuessText ] = useState<string>("");
+    function checkGuess(value : string) : void {
+        const titleMatch = value.length >= MIN_GUESS_LENGTH && currentGame.title.toLowerCase().includes(value.toLocaleLowerCase());
+        setGuessed(titleMatch);
+    };
 
     return <>
         <Game { ...{ ...currentGame, showCompleteDetails: guessed } }/>
         <GuessStatus guessed={ guessed } />
-        <input data-testid="txtGuess" type="text" placeholder="your guess here"
-            value={guessText}
+        <input data-testid="guess-input" type="text" placeholder="your guess here"
+            value={ guessText }
             onChange={ ({ target: { value } }) => setGuessText(value) }
-            onBlur={ ({ target: { value } }) => {
-            const titleMatch = value.length >= MIN_GUESS_LENGTH && currentGame.title.toLowerCase().includes(value.toLocaleLowerCase());
-            setGuessed(titleMatch);
-        } }/>
+            onBlur={ ({ target: { value } }) => checkGuess(value) }/>
         { guessed === true && <button data-testid="next-button" onClick={ reset }>Next!</button> }
     </>;
 };
